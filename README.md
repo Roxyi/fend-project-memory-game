3f721255cdf40974b4aae9017d6803be7231fa65
roxyi <xuyiroy@hotmail.com>
2018-04-14 02:14:03 -0400

    finish the project

--- README.md
+++ README.md
@@ -1,20 +1,28 @@
 # Memory Game Project
 
-## Table of Contents
+## About
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+This is my submission to Udacity Front-End Web Developer Nanodegree's project:
+Memory Game.
 
-## Instructions
+It is based on the starter project which has some HTML and CSS styling to display
+a static version of the Memory Game project.
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+I added a modal in HTML code and corresponding styles in CSS code.
 
-To get started, open `js/app.js` and start building out the app's functionality
+In 'js/app.js', I added DOM functions and event listeners to get the info of
+elements from DOM and change their attributes based on users' behaviors.
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+## Notes
+1. The shuffleCards function only works when you click the restart button. On an
+initial load, the layout is always the same.
+2. The time you spend does not affect the rating you get. I may consider to add
+a list in the modal to show your best score in terms of the time you spend.
+3. When your moves reach 30, you will get 2 stars. When your moves reach 40, you
+will get one star. These two numbers are decided arbitrarily. Technically, when
+there are 16 cards, in the worst scenario, the best performance is 24 moves. I
+added a little bit tolerance to this number, so it is 30.
+4. I don't have much experience with performance analysis. The code is not optimized.
+As I learn more, I will come back optimize the code.
+5. Any bug report is welcome.
 
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
--- js/app.js
+++ js/app.js
@@ -1,28 +1,61 @@
 /*
  * Create a list that holds all of your cards
  */
+const cards = [];
+const cardElements = document.querySelectorAll('.card');
+for (cardElement of cardElements) {
+  cards.push(cardElement.firstElementChild.className);
+}
 
+/*
+ * Get elements from DOM
+ * Initial an openCardList to store selected cards and matached cards
+ * Initial timer, moves, and a toggleTimer flag for starting and ending timer
+ */
+const stars = document.querySelectorAll('.stars');
+const modal = document.querySelector('.modal');
+const secondsElement = document.querySelector('.seconds');
+const movesElement = document.querySelector('.moves');
+const finalSeconds = document.querySelector('#finalSeconds');
+const finalMoves = document.querySelector('#finalMoves');
+let openCardList = [];
+let clickTimes = 0;
+let seconds = 0;
+let toggleTimer = false;
 
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
+ * Notes from Yi Xu: I don't call the shufflecards here in that there is HTML
+ * code displaying the cards on the page. I don't want to waste it. The shuffleCards
+ * function will be called when the game is restarted.
  */
+
+function shuffleCards(cards) {
+  const shuffledCards = shuffle(cards);
+  for (let i = 0; i < cardElements.length; i++) {
+    const cardChild = cardElements[i].firstElementChild;
+    cardChild.className = `fa fa-${shuffledCards[i]}`;
+  }
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 
@@ -36,3 +69,112 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+document.querySelector('.deck').addEventListener('click', clickCard);
+
+function clickCard(e) {
+  const clickedCard = e.target;
+  if (clickedCard.className == 'card') {
+    toggleTimer = true;
+    clickedCard.classList.add('open', 'show');
+    addToOpenCardList(clickedCard);
+    incrementMoves();
+    checkIfWin();
+  }
+}
+
+function addToOpenCardList(obj) {
+  // new cards are always added to the beginning of the array
+  openCardList.unshift(obj);
+  if (openCardList.length % 2 == 0) {
+    const card1 = openCardList[0];
+    const card2 = openCardList[1];
+    if (card1.firstElementChild.className == card2.firstElementChild.className) {
+      cardsAreMatched(card1, card2);
+    } else {
+      cardsAreNotMatched(card1, card2);
+    }
+  }
+}
+
+function cardsAreMatched(card1, card2) {
+  card1.className = 'card match';
+  card2.className = 'card match';
+}
+
+function cardsAreNotMatched(card1, card2) {
+  openCardList.splice(0, 2);
+  // use setTimeout to delay the change of class name
+  setTimeout(() => {
+    card1.className = 'card';
+    card2.className = 'card';
+  }, 500);
+}
+
+function incrementMoves() {
+  clickTimes += 1;
+  movesElement.textContent = clickTimes;
+  if (clickTimes == 30) {
+    for (star of stars) {
+      star.childNodes[1].style.display = 'none';
+    }
+  }
+  if (clickTimes == 40) {
+    for (star of stars) {
+      star.childNodes[3].style.display = 'none';
+    }
+  }
+}
+
+function checkIfWin() {
+  if (openCardList.length == 16) {
+    toggleTimer = false;
+    finalSeconds.textContent = seconds;
+    finalMoves.textContent = clickTimes;
+    modal.style.display = "flex";
+  }
+}
+
+/* a timer will be started if the toggleTimer flag is set to true */
+const timer = setInterval(() => {
+  if (toggleTimer) {
+    seconds += 1;
+    secondsElement.textContent = seconds;
+  } else {
+    // console.log(seconds);
+  }
+}, 1000);
+
+/* when restart button is clicked, assign the initial values to timer, moves,
+ * and openCardList. Flip all cards back and shuffle the cards.
+ */
+document.querySelector('.restart').addEventListener('click', restart);
+
+function restart() {
+  if (clickTimes >= 40) {
+    for (star of stars) {
+      star.childNodes[1].style.display = 'inline-block';
+      star.childNodes[3].style.display = 'inline-block';
+    }
+  } else if (clickTimes >= 30) {
+    for (star of stars) {
+      star.childNodes[1].style.display = 'inline-block';
+    }
+  }
+  toggleTimer = false;
+  seconds = 0;
+  secondsElement.textContent = seconds;
+  clickTimes = 0;
+  movesElement.textContent = clickTimes;
+  for (cardElement of cardElements) {
+    cardElement.className = 'card';
+  }
+  shuffleCards(cards);
+  openCardList = [];
+}
+
+/* Click anywhere out of the modal content will close the modal */
+document.body.addEventListener('click', (e) => {
+  if (e.target.className == 'modal') {
+    modal.style.display = 'none';
+  }
+});
